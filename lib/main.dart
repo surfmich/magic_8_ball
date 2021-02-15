aaa6cd5b5980f93432097694675488fc7b1c5628
Ben <bwillis725@gmail.com>
2021-02-14 19:04:56 -0500

    step 3 ball displays

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,22 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue.shade900,
       ),
       backgroundColor: Colors.blue,
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball1.png'),
     );
   }
 }
